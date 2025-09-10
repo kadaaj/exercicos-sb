fa9d2c67ac9045723b3307c830fa3e5f1b6ca654
Gabriel <gabrielkadaaj@yahoo.com.br>
2025-09-10 16:33:54 -0300

    crud1

--- src/main/java/br/com/cod3r/exercicos_sb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exercicos_sb/controllers/ProdutoController.java
@@ -6,6 +6,8 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
@@ -22,5 +24,16 @@ public class ProdutoController {
     @GetMapping
     public Iterable<Produto> obterProdutos(){
         return produtoRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Produto> obterProdutoPorId(@PathVariable int id){
+        return produtoRepository.findById(id);
+    }
+
+    @PutMapping
+    public Produto alterarProduto(@Valid Produto produto){
+        produtoRepository.save(produto);
+        return produto;
     }
 }
