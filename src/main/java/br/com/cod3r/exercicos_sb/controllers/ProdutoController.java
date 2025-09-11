a05cc1f453183e5b9fd064ba7daa7673bd4bd8ee
Gabriel <gabrielkadaaj@yahoo.com.br>
2025-09-11 18:22:41 -0300

    crud2

--- src/main/java/br/com/cod3r/exercicos_sb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exercicos_sb/controllers/ProdutoController.java
@@ -15,7 +15,7 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @PostMapping
+    @RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
     public @ResponseBody Produto novoProduto(@Valid Produto produto){
         produtoRepository.save(produto);
         return produto;
@@ -26,14 +26,24 @@ public class ProdutoController {
         return produtoRepository.findAll();
     }
 
+    @GetMapping(path = "/pagina/{numeroPagina}")
+    public Iterable<Produto> obterProdutoPorPagina(@PathVariable int numeroPagina){
+        return produtoRepository.findAll();
+    }
+
     @GetMapping(path = "/{id}")
     public Optional<Produto> obterProdutoPorId(@PathVariable int id){
         return produtoRepository.findById(id);
     }
 
-    @PutMapping
-    public Produto alterarProduto(@Valid Produto produto){
-        produtoRepository.save(produto);
-        return produto;
+//    @PutMapping
+//    public Produto alterarProduto(@Valid Produto produto){
+//        produtoRepository.save(produto);
+//        return produto;
+//    }
+
+    @DeleteMapping(path = "/{id}")
+    public void excluirProduto(@PathVariable int id){
+        produtoRepository.deleteById(id);
     }
 }
